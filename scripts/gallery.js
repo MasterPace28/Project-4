22ec0395e63baa2776d2b073c7d1d4fde47b4542
ZachCreates <zdsteinbrook@gmail.com>
2016-04-11 20:46:06 -0400

    Markup and CSS
    This commit includes markup for the HTML, CSS, and notes for the
    Javascript.

---
+++ scripts/gallery.js
@@ -0,0 +1,19 @@
+// Interactive Media Gallery by @ZachCreates
+// A searchable media gallery and lightbox.
+
+
+// Accept live user input from form input box
+	// Show/Hide images as user types
+// User clicks on desired image
+	//Lightbox opens
+// Lightbox
+	//dark background
+	//centered image
+	//caption at bottom
+	//arrows left and right next to image
+// User can move to next or previous picture by clicking on arrows
+// User can move to next or previous picture by using arrow keys
+// User can exit lightbox by clicking anywhere outside of lightbox
+// Additional Functions
+	// Pictures and Videos (Youtube, Vimeo)
+	// Animation effects when searching gallery
