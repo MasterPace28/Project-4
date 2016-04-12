abcdfad232f2da162d17e2f2c024d91e2bacb853
ZachCreates <zdsteinbrook@gmail.com>
2016-04-11 21:42:54 -0400

    Input from User
    Collects live input from user and displays each change to class search
    box value in console.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,7 +2,25 @@
 // A searchable media gallery and lightbox.
 
 
+
 // Accept live user input from form input box
+var timer = null;
+var x = '';
+
+$('.searchbox').keydown(function(){
+	clearTimeout(timer);
+	timer = setTimeout(userInput, 100)
+});
+
+function userInput() {
+	var x = document.querySelector(".searchbox").value;
+	console.log(x);
+}
+
+
+
+
+
 	// Show/Hide images as user types
 // User clicks on desired image
 	//Lightbox opens
